ca057395e172bbd7334f5564345a3bb0e21e2db0
Angul0 <anguloalan55@gmail.com>
2025-05-02 20:23:27 -0700

    elimino residuo de la pantalla de pruieba

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { LoginComponent } from './app/login/login.component';
 import { RegistroComponent } from './app/registro/registro.component';
 import { BienvenidaComponent } from '@app/bienvenida/bienvenida.component';
-import { PruebaComponent } from '@app/prueba/prueba.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -19,7 +18,6 @@ bootstrapApplication(AppComponent, {
       { path: 'login', component: LoginComponent },
       { path: 'registro', component: RegistroComponent },
       { path: 'bienvenida', component: BienvenidaComponent },
-      { path: 'prueba', component: PruebaComponent }
     ])
   ]
 }).catch(err => console.error(err));
