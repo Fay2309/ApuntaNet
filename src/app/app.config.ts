ed298fc34a518bf08323d33421252f1c6cb39913
GuillermoEAC <guillermochina12@gmail.com>
2025-12-08 23:58:46 -0700

    Paso2  para el front

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,3 @@
-// import { ApplicationConfig, provideZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
-// import { provideRouter, withComponentInputBinding } from '@angular/router';
-
-// import { routes } from './app.routes';
-// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-// import { provideHttpClient, withFetch } from '@angular/common/http';
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideZonelessChangeDetection(),
-//     provideRouter(routes, withComponentInputBinding()),
-//     provideHttpClient(withFetch()),
-//     provideClientHydration(withEventReplay())
-//   ]
-// };
-// src/app/app.config.ts
-
-// 1. Quitamos 'provideZoneChangeDetection' que no se usa
 import {
   ApplicationConfig,
   provideZonelessChangeDetection,
